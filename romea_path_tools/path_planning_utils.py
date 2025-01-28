27ee363ab833390b5c75f9b7ac58ec9f0ced2cb8
Cyrille Pierre <cyrille.pierre@inrae.fr>
2025-01-28 10:38:28 +0100

    improve discretize_swaths()

--- romea_path_tools/path_planning_utils.py
+++ romea_path_tools/path_planning_utils.py
@@ -1,5 +1,6 @@
 import math
-from fields2cover import Path, PathState, Point
+import copy
+from fields2cover import Path, Point
 
 
 def dist(a: Point, b: Point):
@@ -8,44 +9,27 @@ def dist(a: Point, b: Point):
 
 def discretize_swaths(path: Path, step_size: float):
     new_path = Path()
+    prev_state = path.getStates()[0]
 
-    # Loop through all the points in the path
-    for i in range(len(path.states) - 1):
-        start_point = path.states[i].point
-        end_point = path.states[i + 1].point
+    for state in path.getStates():
+        while (distance := dist(prev_state.point, state.point)) > step_size:
+            step_state = copy.copy(prev_state)
+            prev_p, p = prev_state.point, state.point
 
-        distance = dist(end_point, start_point)
-        number_of_steps = math.fabs(distance / step_size)
-        rounded_number_of_steps = round(number_of_steps)
-
-        # If rounded number of steps is equal to zero, then the provided step_size is greater
-        # than the distance. In this case, set the number of steps to 1, thus do not discretize
-        # the path
-        if rounded_number_of_steps == 0:
-            rounded_number_of_steps = 1
-
-        dx_coef = (end_point.getX() - start_point.getX()) / rounded_number_of_steps
-        dy_coef = (end_point.getY() - start_point.getY()) / rounded_number_of_steps
-        dz_coef = (end_point.getZ() - start_point.getZ()) / rounded_number_of_steps
-
-        # Iterate over each pair of coordinates and add them as states into our new Path object
-        for j in range(rounded_number_of_steps + 1):
-            # Create a new PathState object
-            state = PathState()
-
-            # Update point with incremental step values
-            state.point = Point(
-                start_point.getX() + j * dx_coef,
-                start_point.getY() + j * dy_coef,
-                start_point.getZ() + j * dz_coef,
+            step_coef = step_size / distance
+            step_state.point = Point(
+                prev_p.getX() + (p.getX() - prev_p.getX()) * step_coef,
+                prev_p.getY() + (p.getY() - prev_p.getY()) * step_coef,
+                prev_p.getZ() + (p.getZ() - prev_p.getZ()) * step_coef,
             )
+            step_state.len = min(step_size, distance)
 
-            state.angle = path.states[i].angle
-            state.velocity = path.states[i].velocity
-            state.duration = float(distance / state.velocity / rounded_number_of_steps)
-            state.dir = path.states[i].dir
-            state.type = path.states[i].type
+            new_path.addState(step_state)
+            prev_state = step_state
 
-            new_path.states.append(state)
+        if distance > 1e-3:
+            new_path.addState(state)
+
+        prev_state = state
 
     return new_path
